0b683de59e19af9f9c3a8ad88e961108200fa914
mylittlepython <address@server.fake>
2015-09-03 19:49:44 +0300

    case1 existing project

--- hi.py
+++ hi.py
@@ -1,18 +1,25 @@
 #http://webpy.org/
 import web
 
-urls = ('/(.*)/', 'redirect',
-        "/.*", "hi")
+render= web.template.render('templates/')
 
-app = web.application(urls, globals())
+#regex explanation: parenthesis -> create a capture group
+#and indicate precedence
+#dot star -> match 0 or more characters
+urls = ('/(.*)', 'index')
 
-class redirect:
-    def GET(self, path):
-        web.seeother('/' + path)
+#request the webpage context
+class index:
+    def GET(self, name):
+        return render.index(name)
 
-class hi:
-    def GET(self):
-        return 'Hi all'
-
+#let the web library know (in this case web.py
+#how to serve web pages according to the defined urls
+#start the webapp
 if __name__ == "__main__":
+    app = web.application(urls, globals())
     app.run()
+
+#for this simple hello_name app
+#python hi.py
+#localhost:8080/name
